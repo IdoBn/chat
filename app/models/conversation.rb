c33bcd96d90a5b9c0c870cf99559bb1189a2d1c6
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-04-14 16:03:37 +0000

    adding conversations, messages, participants and all associations to go along

---
+++ app/models/conversation.rb
@@ -0,0 +1,5 @@
+class Conversation < ActiveRecord::Base
+	has_many :messages
+	has_many :participants
+	has_many :users, through: :participants
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414122459) do
+ActiveRecord::Schema.define(version: 20140414155744) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "conversations", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "messages", force: true do |t|
+    t.integer  "user_id"
+    t.text     "body",            default: ""
+    t.integer  "conversation_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "participants", force: true do |t|
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,7 +51,7 @@ ActiveRecord::Schema.define(version: 20140414122459) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
