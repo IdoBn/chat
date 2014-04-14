110f9334d9408e223dd1ab7e9efa675ddf8b6f53
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-04-14 11:57:57 +0000

    first commit

---
+++ app/assets/javascripts/chat.js
@@ -0,0 +1,56 @@
+jQuery(function() {
+	var dispatcher = new WebSocketRails($('#chat').data('uri'), true);
+	var private_channel = dispatcher.subscribe('channel');
+
+	var messageQueue = [];
+
+	private_channel.on_success = function(data) {
+		console.log('channel success: ' + data.message);
+	}
+
+	dispatcher.on_open = function(data) {
+		console.log('Connection has been established: ', data);
+	}
+
+	private_channel.bind('new_message', function(message) {
+    console.log('new message: ' + message.msg_body + ', ' + message.user_name + ', ' + message.received);
+    newMessage(message);
+  });
+
+  $('#send').bind('click', function() {
+    sendMessage();
+  })
+
+  function sendMessage() {
+    message = $('#message').val();
+    user_name = $('#user_name').val();
+    dispatcher.trigger ('new_message', {user_name: user_name, msg_body: message});
+    $('#message').val('');
+  }
+
+  function messageTemplate(message) {
+    return "<div class='message' ><label class='label label-info'>[" + message.received + "] " + message.user_name +"</label>&nbsp;"+ message.msg_body +"</div>";
+  }
+
+  function shifMessageQueue() {
+    messageQueue.shift();
+    $('#chat div.messages:first').slideDown(100, function() {
+      $(this).remove();
+    });
+  }
+
+  function newMessage(message) {
+    messageQueue.push(message);
+    if (messageQueue.length > 15) {
+      shiftMessageQueue();
+    }
+    appendMessage(message);
+  }
+
+  function appendMessage(message) {
+    var template = messageTemplate(message);
+    $('#chat').append(template);
+    //template.slideDown(140);
+  }
+
+});
---
+++ app/controllers/chat_controller.rb
@@ -0,0 +1,39 @@
+class ChatController < WebsocketRails::BaseController
+  include ActionView::Helpers::SanitizeHelper
+
+  def initialize_session
+    puts "Session Initialized\n"
+  end
+
+  def system_msg(ev, msg)
+    broadcast_message ev, { 
+      user_name: msg[:user_name], 
+      received: Time.now.to_s(:short), 
+      msg_body: msg[:msg_body]
+    }
+  end
+
+  def private_msg(ev, msg)
+    WebsocketRails[:channel].trigger(ev, {
+        user_name: msg[:user_name],
+        received: Time.now.to_s(:short),
+        msg_body: msg[:msg_body]
+      })
+  end
+
+  def new_message
+		private_msg :new_message, message
+  	puts "new message: #{message}" 
+  end
+
+  def client_connected
+    private_msg :new_message, "client #{client_id} connected"
+    puts "private client #{client_id} connected"
+  end
+
+  def delete_user
+    connection_store[:user] = nil
+    private_msg "client #{client_id} disconnected"
+    broadcast_user_list
+  end
+end
